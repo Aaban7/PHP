2ebe42a4074332825bf6b2c17532f1a338f03cc0
Aaban7 <aabansanjrani059.com>
2026-01-05 15:19:31 +0500

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+$price = 499;
+
+if ($price > 500) {
+    echo "The price is greater than 500";
+} else {
+    echo "The price is less than 500";
+}
+
+$T_marks = 62;
+$attendance = 79;
+
+
+if ($T_marks > 60 && $attendance >= 75) {
+    echo "The student can give the exam";
+} else {
+    echo "The student cannot give the exam";
+}
+
+?>
