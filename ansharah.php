89959af63837a182008903a2e2a29358f01658a2
Aaban7 <Aabansanjrani059@gmail.com>
2026-01-05 15:30:42 +0500

    first commit

---
+++ ansharah.php
@@ -0,0 +1,21 @@
+<?php
+
+$price = 300;
+
+if ($price > 500) {
+    echo "The product price is greater than 500";
+} else {
+    echo "The product price is less than 500";
+}
+
+ 
+$tot_marks = 64;
+$attendence = 86;
+
+if ($tot_marks > 60 & $attendence > 75) {
+    echo"The student is eligible for the exams";
+} else {
+    echo "The student is not eligible for the exams";
+}
+
+?>
